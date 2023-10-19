d4d138d7921eb95cef66de920b66cc8540255297
uttuag <uttuag10@gmail.com>
2023-10-19 22:49:24 +0530

    Add components

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-
+import { GiShoppingBag } from "react-icons/gi";
 const Header = () => {
   return (
     <>
@@ -19,6 +19,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
+              <GiShoppingBag />
               MERNCart
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
