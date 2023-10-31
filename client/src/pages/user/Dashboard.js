350995c0badf1e5a51fbb18834f99d767f6f013b
uttuag <uttuag10@gmail.com>
2023-10-31 23:59:42 +0530

    Changes In Components

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Layout from "./../../components/layout";
-
+import UserMenu from "../../components/Layout/UserMenu";
 import { useAuth } from "../../context/auth";
 const Dashboard = () => {
   const [auth] = useAuth();
   return (
-    <Layout title={"Dashboard - Ecommerce App"}>
+    <Layout title={"Dashboard - MERNCart"}>
       <div className="container-flui m-3 p-3 dashboard">
         <div className="row">
-          <div className="col-md-3"></div>
+          <div className="col-md-3">
+            <userMenu />
+          </div>
           <div className="col-md-9">
             <div className="card w-75 p-3">
               <h3>{auth?.user?.name}</h3>
